4eb2955f85ae041b75ad33e6941699ce72ee4be3
sajeel <you@example.com>
2024-06-05 21:33:59 +0500

    first commit

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,52 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :authorized
+
+  # GET /notes or /notes.json
+  def index
+    @notes = Note.where user: @user.id
+
+    render json: @notes
+  end
+
+  # POST /notes or /notes.json
+  def create
+    puts "in the create method"
+    @note = Note.new(note_params)
+    puts "note params #{note_params}"
+    puts "notes object #{@note}"
+    @note.user = @user
+
+      if @note.save
+        render json: @note, status: :created, location: @note
+      else
+         render json: @note.errors, status: :unprocessable_entity
+      end
+
+  end
+
+  # PATCH/PUT /notes/1 or /notes/1.json
+  def update
+      if @note.update(note_params)
+        render json: :@note, status: :ok, location: @note
+      else
+        render json: @note.errors, status: :unprocessable_entity
+      end
+  end
+
+  # DELETE /notes/1 or /notes/1.json
+  def destroy
+    @note.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def note_params
+      params.require(:note).permit(:message, :user_id)
+    end
+end
